b75dfb69f3cc287687602ab62a193cbad958578c
Blake Zajac <contact@blakezajac.com>
2026-01-23 23:23:21 +1100

    Add astro-lenis

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+
+import mdx from '@astrojs/mdx'
+import lenis from 'astro-lenis';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [mdx()],
+    integrations: [mdx(), lenis()],
 });
