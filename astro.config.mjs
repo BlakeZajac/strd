711592e0395c516d2fa025b86c4eee7a9b886b1f
Blake Zajac <contact@blakezajac.com>
2026-01-24 07:49:56 +1100

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 import mdx from '@astrojs/mdx'
 import lenis from 'astro-lenis';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [mdx(), lenis()],
+    site: 'https://blakezajac.com',
+    integrations: [mdx(), lenis(), sitemap()],
 });
