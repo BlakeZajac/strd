e799cbaa1a094ea55004ac65c5c591ba1c1547ac
Blake Zajac <contact@blakezajac.com>
2026-03-08 09:48:02 +1100

    Remove lenis; remove card transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx'
-import lenis from 'astro-lenis';
-import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel';
-
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: vercel(),
-    site: 'https://blakezajac.com',
-    integrations: [mdx(), lenis(), sitemap()],
+  output: "server",
+  adapter: vercel(),
+  site: "https://blakezajac.com",
+  integrations: [mdx(), sitemap()],
 });
